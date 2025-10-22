f08aa9b35b16c94097bbba4cf5381f4b67a7c1ae
Dendi <63695824+Dendi6@users.noreply.github.com>
2025-10-22 14:42:21 +0700

    feat : testing for laravel helper install command

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace VendorName\MyPackage\Console;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    protected $signature = 'labkito-helper:install';
+    protected $description = 'Install package, Inertia dependencies, and custom frontend assets';
+
+    public function handle()
+    {
+        $this->info('Installing package dependencies via Composer...');
+        // $this->runShellCommand('composer require @inertiajs/inertia-laravel');
+
+        $this->info('Installing frontend dependencies via npm...');
+        // $this->runShellCommand('npm install @inertiajs/inertia @inertiajs/inertia-vue3 vue@3');
+
+        $this->info('Installing custom package frontend assets...');
+        // $this->runShellCommand('npm install your-custom-package');
+        $this->info('Publishing package assets...');
+        // $this->call('vendor:publish', ['--tag' => 'mypackage-js']);
+        $this->info('Installation complete!');
+    }
+
+    protected function runShellCommand($command)
+    {
+        $process = proc_open($command, [
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w'],
+        ], $pipes);
+
+        if (is_resource($process)) {
+            while ($line = fgets($pipes[1])) {
+                echo $line;
+            }
+            while ($err = fgets($pipes[2])) {
+                echo $err;
+            }
+
+            proc_close($process);
+        }
+    }
+}
---
+++ src/LaravelHelperServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+namespace LabKito\LaravelHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHelperServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        foreach (glob(__DIR__ . '/Helpers/*.php') as $filename) {
+            require_once $filename;
+        }
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \LabKito\LaravelHelper\Console\InstallCommand::class,
+            ]);
+
+            // $this->publishes([
+            //     __DIR__.'/../resources/js' => resource_path('vendor/mypackage'),
+            // ], 'mypackage-js');
+        }
+    }
+
+    public function register()
+    {
+        //
+    }
+}
